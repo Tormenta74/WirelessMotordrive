b1b97756ae3d5f63a0795415b956cfc7f7a1052a
Diego Sáinz de Medrano <diego.sainz101@alu.ulpgc.es>
2017-06-24 12:57:02 +0100

    Changed directory structure. Added initial Xbee connectivity code (taken from super_xbee).

--- Makefile
+++ Makefile
@@ -1,3 +1,3 @@
 
-test_console: console/test.c console/controls.c console/timing.c
+test_console: src/test.c src/controls.c src/timing.c
 	gcc $^ -Iinclude/ -lncurses -o test 
--- include/connect.h
+++ include/connect.h
@@ -2,6 +2,9 @@
 #ifndef _CONNECT_H
 #define _CONNECT_H
 
+#define MAX_STR_LENGTH 72 // according to xbee-arduino documentation
+
+void setup();
 int connect(char *port);
 int send(char *msg);
 char *receive();
